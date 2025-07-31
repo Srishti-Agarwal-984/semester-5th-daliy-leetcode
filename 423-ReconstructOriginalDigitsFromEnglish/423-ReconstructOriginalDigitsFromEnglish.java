48d70af27b3b3a73b12b8371aacad8eb4f404418
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-07-31 17:34:37 +0530

    Create 423-ReconstructOriginalDigitsFromEnglish

---
+++ 423-ReconstructOriginalDigitsFromEnglish/423-ReconstructOriginalDigitsFromEnglish.java
@@ -0,0 +1,25 @@
+// Last updated: 7/31/2025, 5:34:36 PM
+class Solution {
+    public String originalDigits(String s) {
+        int[] count = new int[26];
+        for (int i=0; i<s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        int[] num = new int[10];
+        num[0] = count['z' - 'a']; 
+        num[2] = count['w' - 'a']; 
+        num[4] = count['u' - 'a']; 
+        num[6] = count['x' - 'a'];
+        num[8] = count['g' - 'a']; 
+        num[3] = count['h' - 'a'] - num[8]; 
+        num[5] = count['f' - 'a'] - num[4]; 
+        num[7] = count['s' - 'a'] - num[6]; 
+        num[1] = count['o' - 'a'] - (num[0] + num[2] + num[4]); 
+        num[9] = count['i' - 'a'] - (num[5] + num[6] + num[8]); 
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            result.append(String.valueOf(i).repeat(num[i])); 
+        }
+        return result.toString();
+    }
+}
