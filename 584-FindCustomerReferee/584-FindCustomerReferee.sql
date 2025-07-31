b3d61a78b68b2a9fff116eb728d65a1d7f670f2e
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-07-31 17:33:47 +0530

    Create 584-FindCustomerReferee

---
+++ 584-FindCustomerReferee/584-FindCustomerReferee.sql
@@ -0,0 +1,4 @@
+-- Last updated: 7/31/2025, 5:33:45 PM
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select name from customer where referee_id != 2 or referee_id is null;
