1ddba5c4b2ae01177d07c931a2fba26272668ce7
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-07-31 17:32:14 +0530

    Create 1054-ComplementOfBase10Integer

---
+++ 1054-ComplementOfBase10Integer/1054-ComplementOfBase10Integer.java
@@ -0,0 +1,43 @@
+// Last updated: 7/31/2025, 5:32:12 PM
+class Solution {
+    public int bitwiseComplement(int n) {
+        int num= n;
+        int g = 0;
+        int c=0;
+        if(n==0){
+            return 1;
+        }
+        String as = new String();
+        while(num!=0){
+            if(num%2==0){
+                as=as+"1";
+            }
+            else{
+                as=as+"0";
+            }
+            num=num/2;
+            c++;
+            
+        }
+        int i=0;
+        while(c!=0){
+            int b = (int)(as.charAt(c-1))-48;
+            
+            g=g+(b*(int)(Math.pow(2,(c-1))));
+            c--;
+        }
+        return g;
+        // int gp = (int)(Math.pow(2,c))-1;
+        // int y=0;
+        // while(gp!=0){
+        //     if((gp&g)==0){
+        //         y=gp;
+        //         break;
+        //     }
+        //     gp--;
+        // }
+        // return y;
+        
+    }
+}
+        
