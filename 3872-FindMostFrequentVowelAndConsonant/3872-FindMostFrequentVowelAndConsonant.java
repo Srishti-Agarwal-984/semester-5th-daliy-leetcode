b70ee9712d4b3c9999e803c263726fb986247ae6
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-07-31 17:25:53 +0530

    Create 3872-FindMostFrequentVowelAndConsonant

---
+++ 3872-FindMostFrequentVowelAndConsonant/3872-FindMostFrequentVowelAndConsonant.java
@@ -0,0 +1,22 @@
+// Last updated: 7/31/2025, 5:25:51 PM
+class Solution {
+    public int maxFreqSum(String s) {
+        int[] ar = new int[26];
+        for(int i=0; i<s.length(); i++){
+            ar[s.charAt(i)-'a']++;
+        }
+        int y=0, l=0;
+        String kl = "aeiou";
+        for(int i=0; i<s.length(); i++){
+            if(kl.indexOf(s.charAt(i))>=0){
+                y=Math.max(ar[s.charAt(i)-'a'],y);
+            }
+            else{
+                l=Math.max(ar[s.charAt(i)-'a'],l);
+            }
+        }
+        return y+l;
+        
+        
+    }
+}
