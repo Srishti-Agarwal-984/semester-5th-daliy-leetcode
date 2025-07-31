f8307f4162886d0a241b1452cd9738064a2a0a04
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-07-31 17:37:56 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.py
@@ -0,0 +1,14 @@
+# Last updated: 7/31/2025, 5:37:54 PM
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reachable = 0
+        for i, num in enumerate(nums):
+            if i > max_reachable:
+                return False
+            max_reachable = max(max_reachable, i + num)
+            if max_reachable >= len(nums) - 1:
+                return True
+        return False
+        
+
+        
