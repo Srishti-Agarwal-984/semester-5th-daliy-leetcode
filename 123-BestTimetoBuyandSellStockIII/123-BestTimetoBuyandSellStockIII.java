b586799be4aeee8a9474afa9908eb5e33938910f
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-08-10 00:09:10 +0530

    Create 123-BestTimetoBuyandSellStockIII

---
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
@@ -0,0 +1,27 @@
+// Last updated: 8/10/2025, 12:09:09 AM
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int k=2;
+        int[][][] dp = new int[n+1][2][k+1];
+        for(int i = n-1; i>=0; i--){
+            for(int buy = 0; buy<=1; buy++){
+                for(int cap = 1; cap<=k; cap++){
+                    int profit = 0;
+                    if(buy == 1){
+                        int take = -prices[i]+dp[i+1][0][cap];
+                        int notake = dp[i+1][1][cap];
+                        profit = Math.max(take,notake);
+                    }
+                    else{
+                        int take = prices[i]+dp[i+1][1][cap-1];
+                        int notake = dp[i+1][0][cap];
+                        profit = Math.max(take,notake);
+                    }
+                    dp[i][buy][cap] = profit;
+                }
+            }
+        }
+        return dp[0][1][k];
+    }
+}
