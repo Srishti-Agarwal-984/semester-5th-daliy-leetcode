48722493bd08693e22a71c27bcabbcb20e41a1aa
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-09-07 11:35:37 +0530

    Create 1304-FindNUniqueIntegersSumuptoZero

---
+++ 1304-FindNUniqueIntegersSumuptoZero/1304-FindNUniqueIntegersSumuptoZero.java
@@ -0,0 +1,27 @@
+// Last updated: 9/7/2025, 11:35:35 AM
+class Solution {
+    public int[] sumZero(int n) {
+        if(n%2!=0){
+            int[] arr = new int[n];
+            int a=(n-1)/2;
+            int b=-a;
+            for(int i=0; i<n; i++){
+                arr[i]=b;
+                b++;
+            }
+            return arr;
+        }
+        int[] arr = new int[n];
+        int a=(n)/2;
+        int b=-a;
+        for(int i=0; i<n; i++){
+            if(b==0){
+                b++;
+            }
+            arr[i]=b;
+            b++;
+        }
+        return arr;
+        
+    }
+}
