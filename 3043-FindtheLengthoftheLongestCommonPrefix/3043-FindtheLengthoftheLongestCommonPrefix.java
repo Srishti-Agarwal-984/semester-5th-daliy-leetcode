65c0328a79a5db4c2c1c3fefe3a63f1fc710e427
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-09-17 00:28:27 +0530

    File updated at 9/17/2025, 12:28:24 AM

--- 3043-FindtheLengthoftheLongestCommonPrefix/3043-FindtheLengthoftheLongestCommonPrefix.java
+++ 3043-FindtheLengthoftheLongestCommonPrefix/3043-FindtheLengthoftheLongestCommonPrefix.java
@@ -1,32 +1,23 @@
-// Last updated: 9/17/2025, 12:27:17 AM
+// Last updated: 9/17/2025, 12:28:24 AM
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        HashSet<String> set = new HashSet<>();
-        for(int b: arr1){
-            String l= String.valueOf(b);
-            StringBuilder sb= new StringBuilder();
-            for(int i=0; i<l.length(); i++){
-                sb.append(l.charAt(i));
-                set.add(sb.toString());
+        HashSet<Integer> st = new HashSet<>();
+        for(int el : arr1){
+            while(!st.contains(el) && el > 0){
+                st.add(el);
+                el/=10;
             }
         }
-        int ans=0;
-        for(int b: arr2){
-            String l= String.valueOf(b);
-            StringBuilder sb= new StringBuilder();
-            for(int i=0; i<l.length(); i++){
-                sb.append(l.charAt(i));
-                //System.out.println(s);
-                if(set.contains(sb.toString())){
-                    ans=Math.max(ans, sb.length());
-                }
-                else{
-                    break;
-                }
+
+        int ans = 0;
+        for(int el : arr2){
+            while(!st.contains(el) &&  el > 0){
+                el/=10;
+            }
+            if(el  > 0){
+                ans = Math.max(ans,(int)(Math.log10(el))+1);
             }
         }
         return ans;
-
-        
     }
 }
