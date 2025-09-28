1b24ca791e8df1069752bcb5f07ba32164c3837b
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-09-28 10:15:26 +0530

    File updated at 9/28/2025, 10:15:24 AM

--- 3698-SplitArrayWithMinimumDifference/3698-SplitArrayWithMinimumDifference.java
+++ 3698-SplitArrayWithMinimumDifference/3698-SplitArrayWithMinimumDifference.java
@@ -1,37 +1,32 @@
-// Last updated: 9/28/2025, 10:14:51 AM
+// Last updated: 9/28/2025, 10:15:24 AM
 class Solution {
     public long splitArray(int[] nums) {
-        int si=0;
-        long sum=0, l=0;
-        for(si=0; si<nums.length-1; si++){
-            if(nums[si]<nums[si+1]){
-                sum+=nums[si];
-            }
-            else{
-                break;
-            }
+        int i = 1, n = nums.length;
+        long suml = nums[0];
+        long sumr = 0;
+        while(i<n && nums[i]>nums[i-1]){
+            suml += nums[i];
+            i++;
         }
-        int r=si;
-        long p=0;
-        if(si+1!=nums.length && nums[si]==nums[si+1]){
-            r++;
 
-        }
-        for(int i=r; i<nums.length-1; i++){
-            if(nums[i]>nums[i+1]){
-                System.out.println(nums[i]);
-                p+=nums[i+1];
-            }
-            else{
-                return -1;
-            }
-            
-        }
-        long ans= Math.min(Math.abs(sum+nums[r]-p), Math.abs(sum-nums[r]-p));
-        if(r!=si){
-            ans=Math.abs(sum-p);
+        if(i==n) return Math.abs(suml-nums[n-1]-nums[n-1]);
+        
+        // System.out.println(i);
+        int pivot = nums[i]==nums[i-1]?0:nums[i-1];
+        sumr = nums[i];
+        i+=1;
 
+        while(i<n && nums[i]<nums[i-1]){
+            sumr+=nums[i];
+            i++;
         }
-        return ans;
+
+        if(i!=n) return -1;
+
+        if(suml<=sumr) return sumr-suml;
+        else {
+            if(suml-sumr-2*pivot>0) return suml-sumr-2*pivot;
+            else return Math.min(suml-sumr,Math.abs(suml-sumr-2*pivot));
+        }
     }
 }
