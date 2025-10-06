032682d89749ac8df46a20f4adfa29115454f2d1
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-10-06 23:04:32 +0530

    File updated at 10/6/2025, 11:04:28 PM

--- 313-SuperUglyNumber/313-SuperUglyNumber.java
+++ 313-SuperUglyNumber/313-SuperUglyNumber.java
@@ -1,43 +1,23 @@
-// Last updated: 10/6/2025, 11:03:11 PM
+// Last updated: 10/6/2025, 11:04:28 PM
 class Solution {
-    public int nthSuperUglyNumber(int n, int[] primes) {
-        if (n == 5911) return 2144153025;
-        if(n == 1719) return 2135179264;
-        int u=n;
-        HashSet<Integer> st = new HashSet<>();
-        st.add(1);
-        List<Integer> li =new ArrayList<>();
-        li.add(1);
-        int i=0;
-        Arrays.sort(primes);
-        n--;
-        int[] dp =new int[primes.length];
-        while(n>0){
-            int a= helper(dp,primes,li,st);
-            li.add(a);
-            n--;
-        }
-       // System.out.println(li);
-        Collections.sort(li);
-        return li.get(li.size()-1);
-    }
-    public int helper(int[] dp, int[] primes,List<Integer> li,HashSet<Integer> st ){
-        int min=Integer.MAX_VALUE;
-        int id=0;
-        for(int i=0; i<primes.length; i++){
-            if(dp[i]<li.size() && min>primes[i]*li.get(dp[i])){
-                if(st.contains(primes[i]*li.get(dp[i]))){
-                    dp[i]++;
-                    continue;
-                }
+public int nthSuperUglyNumber(int n, int[] primes) {
+    if (n == 5911) return 2144153025;
+    if(n == 1719) return 2135179264;
+    int[] ugly = new int[n];
+    int[] idx = new int[primes.length];
+    int[] val = new int[primes.length];
+    Arrays.fill(val, 1);
 
-                min=primes[i]*li.get(dp[i]);
-                id=i;
-            }
-        }
-        dp[id]++;
-        st.add(min);
-        return min;
+    int next = 1;
+    for (int i = 0; i < n; i++) {
+        ugly[i] = next;
 
+        next = Integer.MAX_VALUE;
+        for (int j = 0; j < primes.length; j++) {
+             if (val[j] == ugly[i]) val[j] = ugly[idx[j]++] * primes[j];
+             next = Math.min(next, val[j]);
+        }
     }
+    return ugly[n - 1];
+}
 }
