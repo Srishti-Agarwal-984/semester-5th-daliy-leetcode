f07c41d81ea65d29b94285a0f3d051d8a255358a
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-10-07 12:42:52 +0530

    File updated at 10/7/2025, 12:42:50 PM

--- 3196-MaximizeTotalCostofAlternatingSubarrays/3196-MaximizeTotalCostofAlternatingSubarrays.java
+++ 3196-MaximizeTotalCostofAlternatingSubarrays/3196-MaximizeTotalCostofAlternatingSubarrays.java
@@ -1,26 +1,15 @@
-// Last updated: 10/7/2025, 12:41:39 PM
+// Last updated: 10/7/2025, 12:42:50 PM
 class Solution {
     public long maximumTotalCost(int[] nums) {
-        long[][] dp= new long[nums.length][2];
-        for(long[] p: dp){
-            Arrays.fill(p, Long.MIN_VALUE);
+        long pos = (long) -1e15; // very small → because first element cannot be added directly in pos state
+        long neg = 0; // start from 0
+        long tmp;
+        for (int a : nums) {
+            tmp = neg; // save previous neg
+            neg = pos - a; // if we subtract a, previous must have ended in '+' → pos - a
+            pos = Math.max(pos, tmp) + a; // if we add a, take max of continuing pos or starting fresh from previous neg
+
         }
-        long ans=nums[0];
-        ans=ans+helper(nums,1,1,dp);
-        return ans;
-        
-    }
-    public long helper(int[] nums,int i,int flag,long[][] dp){
-        if(i>=nums.length){
-            return 0;
-        }
-        if(dp[i][flag]!=Long.MIN_VALUE){
-            return dp[i][flag];
-        }
-        long inc=0,exc=0;
-        int a = flag%2!=0?-1:1;
-        inc=inc+nums[i]*a+helper(nums,i+1,(flag+1)%2,dp);
-        exc=nums[i]+helper(nums,i+1,flag,dp);
-        return dp[i][flag]=Math.max(inc,exc);
+        return Math.max(pos, neg);
     }
 }
