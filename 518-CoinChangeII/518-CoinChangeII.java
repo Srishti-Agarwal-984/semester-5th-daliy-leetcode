00e2edb81c593f8da4579425a6a8a3550f0eb23f
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-10-15 17:11:33 +0530

    Create 518-CoinChangeII

---
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -0,0 +1,15 @@
+// Last updated: 10/15/2025, 5:11:32 PM
+class Solution {
+    public int change(int amount, int[] coins) {
+         int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        
+        for (int c : coins) {
+            for (int a = c; a <= amount; a++) {
+                dp[a] += dp[a - c];
+            }
+        }
+        
+        return dp[amount];  
+    }
+}
