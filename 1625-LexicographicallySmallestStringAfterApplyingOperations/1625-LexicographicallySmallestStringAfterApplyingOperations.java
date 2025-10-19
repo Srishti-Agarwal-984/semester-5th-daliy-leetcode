4cdcb1e722ea8d63e82c1741540919eee7b127ec
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-10-20 00:09:33 +0530

    File updated at 10/20/2025, 12:09:29 AM

--- 1625-LexicographicallySmallestStringAfterApplyingOperations/1625-LexicographicallySmallestStringAfterApplyingOperations.java
+++ 1625-LexicographicallySmallestStringAfterApplyingOperations/1625-LexicographicallySmallestStringAfterApplyingOperations.java
@@ -1,25 +1,30 @@
-// Last updated: 10/20/2025, 12:02:57 AM
+// Last updated: 10/20/2025, 12:09:29 AM
 class Solution {
-    public String findLexSmallestString(String s, int a, int b) {
-        Set<String> vis = new HashSet<>();
-        String smallest = s;
-        Deque<String> q = new ArrayDeque<>();
-        q.offer(s);
-        vis.add(s);
-
-        while (!q.isEmpty()) {
-            String cur = q.poll();
-            if (cur.compareTo(smallest) < 0) smallest = cur;
-
-            StringBuilder sb = new StringBuilder(cur);
-            for (int i = 1; i < sb.length(); i += 2)
-                sb.setCharAt(i, (char) ((sb.charAt(i) - '0' + a) % 10 + '0'));
-            String added = sb.toString();
-            if (vis.add(added)) q.offer(added);
-
-            String rotated = cur.substring(cur.length() - b) + cur.substring(0, cur.length() - b);
-            if (vis.add(rotated)) q.offer(rotated);
+    public String add(String s,int a){
+        char[] arr=s.toCharArray();
+        for(int i=1;i<s.length();i=i+2){
+            arr[i] = (char) (((arr[i] - '0' + a) % 10) + '0');
         }
-        return smallest;
+        return String.valueOf(arr);
+    }
+    public String rotate(String s,int b){
+        int j=s.length()-b;
+        String x=s.substring(j);
+        String y=s.substring(0,j);
+        return x+y;
+    }
+    Set<String> seen=new HashSet<>();
+    public String findLexSmallestString(String s, int a, int b) {
+        dfs(s,a,b);
+        return Collections.min(seen);
+    }
+    public void dfs(String s,int a,int b){
+        if (seen.contains(s)){
+            return;
+        }
+        seen.add(s);
+        dfs(add(s,a),a,b);
+        dfs(rotate(s,b),a,b);
+        return;
     }
 }
