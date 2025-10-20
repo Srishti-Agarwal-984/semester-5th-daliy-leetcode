48720641d0ea7046fd07b0f1002d5b50c173887f
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-10-20 12:40:12 +0530

    File updated at 10/20/2025, 12:40:12 PM

--- 956-TallestBillboard/956-TallestBillboard.java
+++ 956-TallestBillboard/956-TallestBillboard.java
@@ -1,28 +1,23 @@
-// Last updated: 10/20/2025, 12:38:27 PM
+// Last updated: 10/20/2025, 12:40:12 PM
 class Solution {
+Integer memo[][];
     public int tallestBillboard(int[] rods) {
-        int n = rods.length;
-        int[][] dp = new int[n][10003];
-        for(int[] a : dp){
-            Arrays.fill(a,-1);
-        }
-        return helper(rods, 0,0,dp)/2;
+        memo=new Integer[rods.length][10002];
+        return tallestBillboard(rods,0,0);
     }
-    public int helper(int[] rods, int idx, int diff, int[][] dp){
-        if(idx >= rods.length){
-            if(diff == 0){
-                return 0;
-            }
-            return Integer.MIN_VALUE;
+    public int tallestBillboard(int[] rods,int si, int diff) {
+        if(si>=rods.length){
+             if(diff==0){
+                 return 0;
+             }
+             return Integer.MIN_VALUE;
         }
-
-        if(dp[idx][diff+5000] != -1){
-            return dp[idx][diff+5000];
+        if(memo[si][diff+5001]!=null){
+            return memo[si][diff+5001];
         }
-
-        int notake = helper(rods, idx+1, diff, dp);
-        int take1 = rods[idx]+helper(rods, idx+1, diff+rods[idx],dp);
-        int take2 = rods[idx]+helper(rods, idx+1, diff-rods[idx],dp);
-        return dp[idx][diff+5000] = Math.max(notake,Math.max(take1,take2));
+        int addWithSupport1 = rods[si]+tallestBillboard(rods,si+1,rods[si]+diff); //**
+        int addWithSupport2 = tallestBillboard(rods,si+1,diff-rods[si]);
+        int addWithNone = tallestBillboard(rods,si+1,diff);
+        return memo[si][diff+5001] = Math.max(addWithNone, Math.max(addWithSupport1,addWithSupport2));
     }
 }
