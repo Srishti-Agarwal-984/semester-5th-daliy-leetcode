0d6a7a5aa73b77ce9ae7b8a596225fcc6312010f
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-10-27 22:51:55 +0530

    File updated at 10/27/2025, 10:51:50 PM

--- 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.java
+++ 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.java
@@ -1,8 +1,9 @@
-// Last updated: 10/27/2025, 10:50:18 PM
+// Last updated: 10/27/2025, 10:51:50 PM
 class Solution {
     public int numberOfBeams(String[] bank) {
         int ans=0;
-        List<Integer> li = new ArrayList<>();
+        int prev=0;
+        //List<Integer> li = new ArrayList<>();
         for(String s: bank){
             int cn=0;
             for(int i=0; i<s.length(); i++){
@@ -12,12 +13,14 @@ class Solution {
 
             }
             if(cn>0){
-                li.add(cn);
+                
+                ans=ans+prev*cn;
+                prev=cn;
             }
         }
-        for(int i=1; i<li.size(); i++){
-            ans = ans+li.get(i)*li.get(i-1);
-        }
+        // for(int i=1; i<li.size(); i++){
+        //     ans = ans+li.get(i)*li.get(i-1);
+        // }
         return ans;
     }
 }
