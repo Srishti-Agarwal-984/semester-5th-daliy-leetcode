cf8c402d75ef1a66d1b1d8fe09542f3759c29e1d
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-10-30 10:24:59 +0530

    File updated at 10/30/2025, 10:24:53 AM

--- 1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray/1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java
+++ 1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray/1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java
@@ -1,22 +1,10 @@
-// Last updated: 10/30/2025, 10:23:05 AM
+// Last updated: 10/30/2025, 10:24:53 AM
 class Solution {
     public int minNumberOperations(int[] target) {
-        int ans =0;
-        Stack<Integer> st = new Stack<>();
-        st.push(target[0]);
-        for(int i=1; i<target.length; i++){
-            if(!st.isEmpty() && st.peek()>=target[i]){
-                ans=ans+st.pop()-target[i];
-                while(!st.isEmpty() && st.peek()>=target[i]){
-                    st.pop();
-                }
-            }
-            st.push(target[i]);
+        int min = target[0];
+        for (int i = 1; i < target.length; i++) {
+            min += Math.max(target[i] - target[i - 1], 0);
         }
-        int max=0;
-        while(!st.isEmpty()){
-            max = Math.max(max,st.pop());
-        }
-        return ans+max;
+        return min;
     }
 }
