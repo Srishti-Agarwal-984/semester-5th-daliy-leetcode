b48b092d15e30912da544709b4eca665a7e98448
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-11-04 06:20:06 +0530

    File updated at 11/4/2025, 6:20:06 AM

--- 3318-FindX-SumofAllK-LongSubarraysI/3318-FindX-SumofAllK-LongSubarraysI.java
+++ 3318-FindX-SumofAllK-LongSubarraysI/3318-FindX-SumofAllK-LongSubarraysI.java
@@ -1,48 +1,54 @@
-// Last updated: 11/4/2025, 6:15:50 AM
+// Last updated: 11/4/2025, 6:20:06 AM
 class Solution {
     public int[] findXSum(int[] nums, int k, int x) {
-        int[] ans=new int[nums.length-k+1];
-        for(int j=0; j<=nums.length-k; j++){
-            Pair[] freq = new Pair[51];
-            for(int i=0; i<51; i++){
-                freq[i]=new Pair(i,0);
-            }
-            for(int i=j; i<j+k; i++){
-                freq[nums[i]] = new Pair(nums[i], freq[nums[i]].count+1);
-            }
-            Arrays.sort(freq, new Comparator<Pair>(){
-                @Override
-                public int compare(Pair p1, Pair p2){
-                    if(p1.count==p2.count){
-                        return p2.key-p1.key;
-                    }
-                    return p2.count-p1.count;
-                }
-            });
-            int d=x;
-            for(int i=0; i<51; i++){
-                if(freq[i].count>0 && d>0){
-                    ans[j]+=freq[i].count*freq[i].key;
-                    d--;
-                }
-                else{
-                    break;
-                }
-            }
+        int[] result = new int[nums.length - k + 1];
 
+        for (int i = 0; i < result.length; i++) {
+            int left = i, right = i + k - 1;
+            result[i] = findXSumofSubArray(nums, left, right, x);
         }
-        return ans;
-        
 
-
-        
+        return result;
     }
-    class Pair{
-        int key;
-        int count;
-        public Pair(int key, int count){
-            this.key=key;
-            this.count=count;
+
+    private int findXSumofSubArray(int[] nums, int left, int right, int x) {
+        int sum = 0, distinctCount = 0;
+        int[] freq = new int[51]; 
+
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+            if (freq[nums[i]] == 0) {
+                distinctCount++;
+            }
+            freq[nums[i]]++;
         }
+
+        if (distinctCount < x) {
+            return sum;
+        }
+
+        sum = 0;
+        for (int pick = 0; pick < x; pick++) {
+            int bestFreq = -1;
+            int bestVal = -1;
+
+            for (int val = 50; val >= 1; val--) {
+                if (freq[val] > bestFreq) {
+                    bestFreq = freq[val];
+                    bestVal = val;
+                }
+            }
+
+            if (bestVal != -1) {
+                sum += bestVal * bestFreq;
+                freq[bestVal] = 0;
+            }
+        }
+        
+        return sum;
     }
 }
+// TC : O(n - k + 1) * findXSumofSubArray() => O(n) * findXSumofSubArray()
+// findXSumofSubArray() =>  O(k + 50 * x) ~ O(k + x)
+// Total TC : O(n * (k + x))
+// SC : O(n - k + 1) * O(51) ~ O(n)
