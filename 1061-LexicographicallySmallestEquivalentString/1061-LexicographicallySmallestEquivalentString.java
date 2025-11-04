b23b46b34516b62b5b5ddcd2cb4137945e761743
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-11-04 22:10:45 +0530

    Create 1061-LexicographicallySmallestEquivalentString

---
+++ 1061-LexicographicallySmallestEquivalentString/1061-LexicographicallySmallestEquivalentString.java
@@ -0,0 +1,39 @@
+// Last updated: 11/4/2025, 10:10:45 PM
+class Solution {
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        int n = s1.length();
+        int m = baseStr.length();
+        HashMap<Character,List<Character>> map = new HashMap<>();
+        for(char ch = 'a'; ch <='z'; ch++){
+            map.put(ch,new ArrayList<>());
+        }
+        for(int i = 0; i<n; i++){
+            char a = s1.charAt(i);
+            char b = s2.charAt(i);
+            map.get(a).add(b);
+            map.get(b).add(a);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<m; i++){
+            HashSet<Character> st = new HashSet<>();
+            char min = dfs(map,st,baseStr.charAt(i));
+            sb.append(min);
+        }
+        return sb.toString();
+    }
+    public char dfs(HashMap<Character,List<Character>> map, HashSet<Character> st, char ch){
+        char min = ch;
+        st.add(min);
+        for(char nbrs : map.get(ch)){
+            if(st.contains(nbrs)){
+                continue;
+            }
+            char t = dfs(map,st,nbrs);
+            if(t < min){
+                min = t;
+            }
+        }
+        return min;
+    }
+}
