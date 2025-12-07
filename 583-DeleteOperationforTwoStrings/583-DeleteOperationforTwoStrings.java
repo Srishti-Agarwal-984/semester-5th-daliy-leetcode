6480fd3557aedb7636ba0c4c96bbbebf40749654
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-12-07 06:16:40 +0530

    File updated at 12/7/2025, 6:16:40 AM

--- 583-DeleteOperationforTwoStrings/583-DeleteOperationforTwoStrings.java
+++ 583-DeleteOperationforTwoStrings/583-DeleteOperationforTwoStrings.java
@@ -1,4 +1,4 @@
-// Last updated: 12/7/2025, 6:15:49 AM
+// Last updated: 12/7/2025, 6:16:40 AM
 1class Solution {
 2    public int minDistance(String s1, String s2) {
 3        int[][] dp = new int[s1.length()][s2.length()];
@@ -13,31 +13,26 @@
 12            return 0;
 13        }
 14        if(i==s1.length()){
-15            int r=0;
-16            for(int k=j; k<s2.length(); k++){
-17                r+=1;
-18            }
-19            return r;
-20        }
-21         if(j==s2.length()){
-22            int r=0;
-23            for(int k=i; k<s1.length(); k++){
-24                r+=1;
-25            }
-26            return r;
-27        }
-28        if(dp[i][j]!=-1){
-29            return dp[i][j];
-30        }
-31        if(s1.charAt(i)==s2.charAt(j)){
-32            int ans=helper(s1, s2, i+1, j+1, dp);
-33            return dp[i][j]=ans;
-34        }
-35        int l = helper(s1, s2, i+1, j, dp)+1;
-36        int r = helper(s1, s2, i, j+1, dp)+1;
-37        int ans=Math.min(l,r);
-38        return  dp[i][j]=ans;
-39
-40
-41    }
-42}
+15            int r=s2.length()-j;
+16            
+17            return r;
+18        }
+19         if(j==s2.length()){
+20            int r=s1.length()-i;
+21            return r;
+22        }
+23        if(dp[i][j]!=-1){
+24            return dp[i][j];
+25        }
+26        if(s1.charAt(i)==s2.charAt(j)){
+27            int ans=helper(s1, s2, i+1, j+1, dp);
+28            return dp[i][j]=ans;
+29        }
+30        int l = helper(s1, s2, i+1, j, dp)+1;
+31        int r = helper(s1, s2, i, j+1, dp)+1;
+32        int ans=Math.min(l,r);
+33        return  dp[i][j]=ans;
+34
+35
+36    }
+37}
