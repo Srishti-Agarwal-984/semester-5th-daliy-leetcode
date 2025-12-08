83c5c6419a1c29d8a6251bff7c4fe8485efbf101
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-12-08 06:45:02 +0530

    Create 630-CourseScheduleIII

---
+++ 630-CourseScheduleIII/630-CourseScheduleIII.java
@@ -0,0 +1,28 @@
+// Last updated: 12/8/2025, 6:45:02 AM
+1class Solution {
+2    public int scheduleCourse(int[][] courses) {
+3        Arrays.sort(courses, (c1, c2) -> Integer.compare(c1[1], c2[1]));
+4        // max heap to track the max duration courses
+5        PriorityQueue<Integer> heap = new PriorityQueue<>((d1, d2) -> Integer.compare(d2, d1));
+6        
+7        int time = 0;
+8        for(int[] course: courses) {
+9            int duration = course[0], lastDay = course[1];
+10            time += duration;
+11            heap.add(duration);
+12            // if pass the last day, remove courses these take longest time.
+13            if(time > lastDay) {
+14                time -= heap.poll();
+15            }
+16        }
+17        return heap.size();
+18	}
+19    class Pair{
+20        int d;
+21        int lt;
+22        public Pair(int d, int lt){
+23            this.d=d;
+24            this.lt = lt;
+25        }
+26    }
+27}
