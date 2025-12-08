76a4a8b1c4d396bf3c83f5eb8118b4b98ece4409
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-12-08 17:42:08 +0530

    File updated at 12/8/2025, 5:42:07 PM

--- 630-CourseScheduleIII/630-CourseScheduleIII.java
+++ 630-CourseScheduleIII/630-CourseScheduleIII.java
@@ -1,4 +1,4 @@
-// Last updated: 12/8/2025, 6:45:26 AM
+// Last updated: 12/8/2025, 5:42:07 PM
 1class Solution {
 2    public int scheduleCourse(int[][] courses) {
 3        Arrays.sort(courses, (c1, c2) -> Integer.compare(c1[1], c2[1]));
@@ -17,4 +17,12 @@
 16        }
 17        return heap.size();
 18	}
-19}
+19    class Pair{
+20        int d;
+21        int lt;
+22        public Pair(int d, int lt){
+23            this.d=d;
+24            this.lt = lt;
+25        }
+26    }
+27}
