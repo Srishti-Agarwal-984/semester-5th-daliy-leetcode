f1a22d5cfce89a4d599145fe4e0652c22bae670d
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-12-10 12:27:55 +0530

    Create 319-BulbSwitcher

---
+++ 319-BulbSwitcher/319-BulbSwitcher.java
@@ -0,0 +1,15 @@
+// Last updated: 12/10/2025, 12:27:54 PM
+1class Solution {
+2    public int bulbSwitch(int n) {
+3        int i=1;
+4        int c=0;
+5        while(i*i<=n){
+6            c++;
+7            i++;
+8
+9        }
+10        return c;
+11        
+12        
+13    }
+14}
