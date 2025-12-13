3bb4bc84a0851009a9c9b673d4647537f9a4f608
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-12-13 06:50:00 +0530

    File updated at 12/13/2025, 6:49:57 AM

--- 1963-MinimumNumberofSwapstoMaketheStringBalanced/1963-MinimumNumberofSwapstoMaketheStringBalanced.java
+++ 1963-MinimumNumberofSwapstoMaketheStringBalanced/1963-MinimumNumberofSwapstoMaketheStringBalanced.java
@@ -1,17 +1,17 @@
-// Last updated: 12/13/2025, 6:48:17 AM
+// Last updated: 12/13/2025, 6:49:57 AM
 1class Solution {
 2    public int minSwaps(String s) {
 3        int cl=0;
 4        int op=0;
-5        Stack<Integer> st = new Stack<>();
+5        //Stack<Integer> st = new Stack<>();
 6        for(int i=0; i<s.length(); i++){
-7            if(!st.isEmpty() &&  s.charAt(i)==']'){
-8                st.pop();
+7            if(op>=1 &&  s.charAt(i)==']'){
+8                op--;
 9            }
 10            else if(s.charAt(i)=='['){
-11                st.add(i);
+11                op++;
 12            }
-13            else if(st.isEmpty() && s.charAt(i)==']'){
+13            else if(op==0 && s.charAt(i)==']'){
 14                cl++;
 15            }
 16        }
