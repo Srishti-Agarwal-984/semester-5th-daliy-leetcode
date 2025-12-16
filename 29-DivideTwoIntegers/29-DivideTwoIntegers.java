b6f27caf2eb337e000cd5e9b4712efe71f8448ca
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-12-17 04:45:56 +0530

    File updated at 12/17/2025, 4:45:50 AM

--- 29-DivideTwoIntegers/29-DivideTwoIntegers.java
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.java
@@ -1,12 +1,12 @@
-// Last updated: 7/31/2025, 5:38:24 PM
-class Solution {
-    public int divide(int dividend, int divisor) {
-        int a;
-        if(dividend==-2147483648 && divisor==-1 ){
-            return 2147483647;
-        }
-        a=(dividend/divisor);
-        return a;
-        
-    }
-}
+// Last updated: 12/17/2025, 4:45:50 AM
+1class Solution {
+2    public int divide(int dividend, int divisor) {
+3        int a;
+4        if(dividend==-2147483648 && divisor==-1 ){
+5            return 2147483647;
+6        }
+7        a=(dividend/divisor);
+8        return a;
+9        
+10    }
+11}
