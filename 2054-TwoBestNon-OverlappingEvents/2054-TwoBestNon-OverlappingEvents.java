cb92c0463ba8b9195ad73790ab7b4d58c1a81825
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-12-23 13:28:39 +0530

    File updated at 12/23/2025, 1:28:30 PM

--- 2054-TwoBestNon-OverlappingEvents/2054-TwoBestNon-OverlappingEvents.java
+++ 2054-TwoBestNon-OverlappingEvents/2054-TwoBestNon-OverlappingEvents.java
@@ -1,62 +1,62 @@
-// Last updated: 10/12/2025, 10:30:03 AM
-class Solution {
-    public class tuple {
-        int first;
-        int second;
-        int val;
-
-        public tuple(int first, int second, int val) {
-            this.first = first;
-            this.second = second;
-            this.val = val;
-        }
-    }
-
-    public int maxTwoEvents(int[][] events) {
-        int n = events.length;
-        tuple[] arr = new tuple[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = new tuple(events[i][0], events[i][1], events[i][2]);
-        }
-        Arrays.sort(arr, new Comparator<tuple>() {
-            @Override
-            public int compare(tuple o1, tuple o2) {
-                return o1.first - o2.first;
-            }
-        });
-
-        int[][] dp = new int[n][3];
-        for (int[] a : dp) {
-            Arrays.fill(a, -1);
-        }
-        return helper(arr, 0, 2, dp);
-    }
-    public int helper(tuple[] arr, int idx, int k, int[][] dp){
-        if (idx >= arr.length) {
-            return 0;
-        }
-        if (k == 0) {
-            return 0;
-        }
-        if (dp[idx][k] != -1) {
-            return dp[idx][k];
-        }
-
-        int notake = helper(arr, idx + 1, k, dp);
-        int next = nextidx(arr[idx].second, arr, idx + 1);
-        int take = arr[idx].val + helper(arr, next, k - 1, dp);
-        return dp[idx][k] = Math.max(take, notake);
-        
-    }
-    public int nextidx(int end, tuple[] arr, int idx) {
-        int l = idx, r = arr.length;
-        while (l < r) {
-            int mid = (l + r) / 2;
-            if (arr[mid].first > end)
-                r = mid;
-            else
-                l = mid + 1;
-        }
-        return l;
-    }
-}
+// Last updated: 12/23/2025, 1:28:30 PM
+1class Solution {
+2    public class tuple {
+3        int first;
+4        int second;
+5        int val;
+6
+7        public tuple(int first, int second, int val) {
+8            this.first = first;
+9            this.second = second;
+10            this.val = val;
+11        }
+12    }
+13
+14    public int maxTwoEvents(int[][] events) {
+15        int n = events.length;
+16        tuple[] arr = new tuple[n];
+17        for (int i = 0; i < n; i++) {
+18            arr[i] = new tuple(events[i][0], events[i][1], events[i][2]);
+19        }
+20        Arrays.sort(arr, new Comparator<tuple>() {
+21            @Override
+22            public int compare(tuple o1, tuple o2) {
+23                return o1.first - o2.first;
+24            }
+25        });
+26
+27        int[][] dp = new int[n][3];
+28        for (int[] a : dp) {
+29            Arrays.fill(a, -1);
+30        }
+31        return helper(arr, 0, 2, dp);
+32    }
+33    public int helper(tuple[] arr, int idx, int k, int[][] dp){
+34        if (idx >= arr.length) {
+35            return 0;
+36        }
+37        if (k == 0) {
+38            return 0;
+39        }
+40        if (dp[idx][k] != -1) {
+41            return dp[idx][k];
+42        }
+43
+44        int notake = helper(arr, idx + 1, k, dp);
+45        int next = nextidx(arr[idx].second, arr, idx + 1);
+46        int take = arr[idx].val + helper(arr, next, k - 1, dp);
+47        return dp[idx][k] = Math.max(take, notake);
+48        
+49    }
+50    public int nextidx(int end, tuple[] arr, int idx) {
+51        int l = idx, r = arr.length;
+52        while (l < r) {
+53            int mid = (l + r) / 2;
+54            if (arr[mid].first > end)
+55                r = mid;
+56            else
+57                l = mid + 1;
+58        }
+59        return l;
+60    }
+61}
