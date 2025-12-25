d7a5a9855a5e26162e99490e844e12f8fe5dce9d
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-12-25 08:05:35 +0530

    File updated at 12/25/2025, 8:05:35 AM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,28 +1,22 @@
-// Last updated: 7/31/2025, 5:38:11 PM
-class Solution {
-    public int trap(int[] height) {
-        return trapping(height);
-     
-    }
-    public static int trapping(int[] arr){
-        int n=arr.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        left[0] = arr[0];
-        right[n-1] = arr[n-1];
-        for(int i=1; i<n; i++){
-            left[i]=Math.max(left[i-1],arr[i]);
-        }
-        for(int i=n-2; i>=0; i--){
-            right[i]=Math.max(right[i+1],arr[i]);
-        }
-        int s=0;
-        for(int i=0; i<n; i++){
-            s=s+Math.min(left[i],right[i])-arr[i];
-        }
-        return s;
-    }
-
-    
-    
-}
+// Last updated: 12/25/2025, 8:05:35 AM
+1class Solution {
+2    public int trap(int[] height) {
+3        Stack<Integer> st = new Stack<>();
+4        int sum=0;
+5        int[] area = height;
+6        for(int i=0; i<height.length; i++){
+7            while(!st.isEmpty() && height[st.peek()]<=height[i]){
+8                // System.out.println(st.peek()+ " "+height[st.peek()]);
+9                // sum=sum+(area[st.peek()])*(i-st.peek());
+10                int r = st.pop();
+11                if(!st.isEmpty()){
+12                    int e = st.peek();
+13                    sum=sum+Math.min(height[e]-height[r], height[i]-height[r])*(i-e-1);
+14                }
+15            }
+16            st.add(i);
+17        }
+18        return sum;
+19        
+20    }
+21}
