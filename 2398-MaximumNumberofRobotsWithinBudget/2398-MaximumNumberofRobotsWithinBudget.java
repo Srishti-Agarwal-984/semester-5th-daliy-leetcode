e988e6b0cec7b437e0c081257f2bc71e74298327
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-12-29 17:25:06 +0530

    File updated at 12/29/2025, 5:25:03 PM

--- 2398-MaximumNumberofRobotsWithinBudget/2398-MaximumNumberofRobotsWithinBudget.java
+++ 2398-MaximumNumberofRobotsWithinBudget/2398-MaximumNumberofRobotsWithinBudget.java
@@ -1,65 +1,66 @@
-// Last updated: 12/29/2025, 5:22:07 PM
+// Last updated: 12/29/2025, 5:25:03 PM
 1class Solution {
 2    public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
-3        int n = runningCosts.length;
-4
-5        long[] prefix = new long[n];
-6        prefix[0] = runningCosts[0];
-7        for (int i = 1; i < n; i++) {
-8            prefix[i] = prefix[i - 1] + runningCosts[i];
-9        }
-10
-11        int lo = 0, hi = n, ans = 0;
-12        while (lo <= hi) {
-13            int mid = lo + (hi - lo) / 2;
-14            if (helper(mid, prefix, chargeTimes, runningCosts, budget)) {
-15                ans = mid;
-16                lo = mid + 1;
-17            } else {
-18                hi = mid - 1;
-19            }
-20        }
-21        return ans;
-22    }
-23
-24    public boolean helper(int k, long[] prefix, int[] chargeTimes, int[] runningCosts, long budget) {
-25        if (k == 0) return true;
-26
-27        PriorityQueue<Pair> pq = new PriorityQueue<>(
-28            (a, b) -> b.val - a.val   // max heap
-29        );
-30
-31        // first window
-32        for (int i = 0; i < k; i++) {
-33            pq.add(new Pair(chargeTimes[i], i));
-34        }
-35
-36        long sum = prefix[k - 1];
-37        if (pq.peek().val + k * sum <= budget) return true;
-38
-39        for (int j = k; j < chargeTimes.length; j++) {
-40            pq.add(new Pair(chargeTimes[j], j));
-41
-42            // remove out-of-window elements
-43            while (pq.peek().idx < j - k + 1) {
-44                pq.poll();
-45            }
-46
-47            sum = prefix[j] - prefix[j - k];
-48
-49            if (pq.peek().val + k * sum <= budget) {
+3        long[] prefix = new long[runningCosts.length];
+4        prefix[0]=runningCosts[0];
+5        for(int i=1; i<runningCosts.length; i++){
+6            prefix[i]=prefix[i-1]+runningCosts[i];
+7        }
+8        int r=0, en = runningCosts.length;
+9        int ans = 0;
+10        while(r<=en){
+11            int mid = r+(en-r)/2;
+12            if(helper(mid,prefix, chargeTimes, runningCosts, budget)){
+13                ans = Math.max(ans, mid);
+14                r =mid+1;
+15            }
+16            else{
+17                en = mid-1;
+18            }
+19        }
+20        return ans;
+21
+22        
+23    }
+24    public boolean helper(int temp, long[] prefix, int[] chargeTimes, int[] runningCosts, long budget){
+25        PriorityQueue<Pair> pq = new PriorityQueue<>(new Comparator<Pair>(){
+26            @Override
+27            public int compare(Pair p1, Pair p2){
+28                return p2.val-p1.val;
+29            }
+30        });
+31        if(temp==0){
+32            return true;
+33        }
+34        int i=0;
+35        for(i=0; i<temp; i++){
+36            pq.add(new Pair(chargeTimes[i], i));
+37        }
+38        long q =pq.peek().val+prefix[temp-1]*temp;
+39        if(q<=budget){
+40            return true;
+41        } 
+42        for(int j=i; j<chargeTimes.length; j++){
+43            pq.add(new Pair(chargeTimes[j], j)); 
+44            while( pq.peek().idx<j-temp+1){
+45                pq.poll();
+46            }
+47            long sum = prefix[j] - prefix[j - temp];
+48            long q1 = pq.peek().val+temp*sum;
+49            if(q1<=budget){
 50                return true;
 51            }
 52        }
 53        return false;
-54    }
+54
 55
-56    class Pair {
-57        int val, idx;
-58        Pair(int v, int i) {
-59            val = v;
-60            idx = i;
-61        }
-62    }
-63}
-64
+56    }
+57    class Pair{
+58        int val;
+59        int idx;
+60        public Pair(int val, int idx){
+61            this.val  = val;
+62            this.idx = idx;
+63        }
+64    }
+65}
