218351b36c00b620281cca4ea921fc29784302b9
Srishti Agarwal <srishtiagarwal984@gmail.com>
2025-12-30 18:50:11 +0530

    File updated at 12/30/2025, 6:50:08 PM

--- 996-NumberofSquarefulArrays/996-NumberofSquarefulArrays.java
+++ 996-NumberofSquarefulArrays/996-NumberofSquarefulArrays.java
@@ -1,40 +1,41 @@
-// Last updated: 12/30/2025, 6:44:53 PM
+// Last updated: 12/30/2025, 6:50:08 PM
 1class Solution {
 2    public int numSquarefulPerms(int[] nums) {
-3        Arrays.sort(nums);
-4        boolean[] used = new boolean[nums.length];
-5        int ans = 0;
+3        int ans = 0;
+4        Arrays.sort(nums);
+5        HashSet<Integer> st = new HashSet<>();
 6
-7        for (int i = 0; i < nums.length; i++) {
-8            if (i > 0 && nums[i] == nums[i - 1]) continue;
-9            used[i] = true;
-10            ans += helper(nums, used, nums[i], 1);
-11            used[i] = false;
-12        }
-13        return ans;
-14    }
-15
-16    public int helper(int[] nums, boolean[] used, int prev, int count) {
-17        if (count == nums.length) {
-18            return 1;
-19        }
-20
-21        int ans = 0;
-22        for (int i = 0; i < nums.length; i++) {
-23            if (used[i]) continue;
-24
-25            // skip duplicates
-26            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
-27
-28            int sum = prev + nums[i];
-29            int k = (int) Math.sqrt(sum);
-30            if (k * k == sum) {
-31                used[i] = true;
-32                ans += helper(nums, used, nums[i], count + 1);
-33                used[i] = false;
-34            }
-35        }
-36        return ans;
-37    }
-38}
-39
+7        for(int i=0; i<nums.length; i++){
+8            if(i>0 && nums[i-1]==nums[i]){
+9                continue;
+10            }
+11            
+12            st.add(i);
+13            ans+=helper(nums, nums[i], st);
+14            st.remove(i);
+15        }
+16        return ans;
+17        
+18    }
+19    public int helper(int[] nums, int prev, HashSet<Integer> st){
+20        if(st.size()==nums.length){
+21            return 1;
+22        }
+23        int ans = 0;
+24        for(int i=0; i<nums.length; i++){
+25           if (i > 0 && nums[i] == nums[i-1] && !st.contains(i-1)) continue;
+26
+27            if(st.contains(i)){
+28                continue;
+29            }
+30            int n = nums[i]+prev;
+31            int k =(int)(Math.pow(n,0.5));
+32            if(n==k*k){
+33                st.add(i);
+34                ans+=helper(nums, nums[i], st);
+35                st.remove(i);
+36            }
+37        }
+38        return ans;
+39    }
+40}
