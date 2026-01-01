cde77f1d0aa69416710884ba13dbc7b20544b07d
Srishti Agarwal <srishtiagarwal984@gmail.com>
2026-01-01 10:38:55 +0530

    File updated at 1/1/2026, 10:38:51 AM

--- 66-PlusOne/66-PlusOne.py
+++ 66-PlusOne/66-PlusOne.py
@@ -1,12 +1,12 @@
-# Last updated: 7/31/2025, 5:37:43 PM
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        s=0
-        d=0
-        while(d<len(digits)):
-            s = (s*10) + digits[d]
-            d=d+1
-        b=s+1
-        c=list(map(int,(str(b))))
-        return c
-        
+# Last updated: 1/1/2026, 10:38:51 AM
+1class Solution:
+2    def plusOne(self, digits: List[int]) -> List[int]:
+3        s=0
+4        d=0
+5        while(d<len(digits)):
+6            s = (s*10) + digits[d]
+7            d=d+1
+8        b=s+1
+9        c=list(map(int,(str(b))))
+10        return c
+11        
