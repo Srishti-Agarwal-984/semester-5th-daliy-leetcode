ec01d93d743251f3b8fa49a985d825a8b98b0576
Srishti Agarwal <srishtiagarwal984@gmail.com>
2026-01-02 19:13:51 +0530

    File updated at 1/2/2026, 7:13:46 PM

--- 3112-MinimumTimetoVisitDisappearingNodes/3112-MinimumTimetoVisitDisappearingNodes.java
+++ 3112-MinimumTimetoVisitDisappearingNodes/3112-MinimumTimetoVisitDisappearingNodes.java
@@ -1,59 +1,51 @@
-// Last updated: 1/2/2026, 7:10:36 PM
+// Last updated: 1/2/2026, 7:13:46 PM
 1class Solution {
 2    public int[] minimumTime(int n, int[][] edges, int[] disappear) {
-3
-4        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
-5        for (int i = 0; i < n; i++) map.put(i, new HashMap<>());
-6
+3        HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
+4        for(int i=0; i<n; i++){
+5            map.put(i, new HashMap<>());
+6        }
 7        for (int[] e : edges) {
 8            int u = e[0], v = e[1], w = e[2];
 9            map.get(u).put(v, Math.min(map.get(u).getOrDefault(v, Integer.MAX_VALUE), w));
 10            map.get(v).put(u, Math.min(map.get(v).getOrDefault(u, Integer.MAX_VALUE), w));
 11        }
-12
-13        int[] ans = new int[n];
-14        Arrays.fill(ans, Integer.MAX_VALUE);
-15
-16        if (disappear[0] == 0) {
-17            Arrays.fill(ans, -1);
-18            return ans;
-19        }
-20
-21        PriorityQueue<Pair> pq = new PriorityQueue<>(
-22            (a, b) -> Integer.compare(a.val, b.val)
-23        );
-24
-25        ans[0] = 0;
-26        pq.add(new Pair(0, 0));
-27
-28        while (!pq.isEmpty()) {
-29            Pair cur = pq.poll();
-30
-31            if (cur.val > ans[cur.ei]) continue;
-32            if (cur.val >= disappear[cur.ei]) continue;
-33
-34            for (int nei : map.get(cur.ei).keySet()) {
-35                int newVal = cur.val + map.get(cur.ei).get(nei);
-36                if (newVal < ans[nei] && newVal < disappear[nei]) {
-37                    ans[nei] = newVal;
-38                    pq.add(new Pair(nei, newVal));
-39                }
-40            }
-41        }
-42
-43        for (int i = 0; i < n; i++) {
-44            if (ans[i] == Integer.MAX_VALUE) ans[i] = -1;
-45        }
-46
-47        return ans;
-48    }
-49
-50    class Pair {
-51        int ei, val;
-52        Pair(int ei, int val) {
-53            this.ei = ei;
-54            this.val = val;
-55        }
-56    }
-57}
-58
+12        int[] ans = new int[n];
+13        Arrays.fill(ans, Integer.MAX_VALUE);
+14        ans[0]=0;
+15        HashSet<Integer> vi = new HashSet<>();
+16        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b)->(a.val-b.val));
+17        pq.add(new Pair(0, 0));
+18        while(!pq.isEmpty()){
+19            Pair ed = pq.poll();
+20            if(vi.contains(ed.ei)){
+21                continue;
+22            }
+23            vi.add(ed.ei);
+24            ans[ed.ei]=ed.val;
+25            for(int k: map.get(ed.ei).keySet()){
+26                int new_val = map.get(ed.ei).get(k)+ans[ed.ei];
+27                if(!vi.contains(k) && ans[k]>new_val && new_val<disappear[k]){
+28                    pq.add(new Pair(k, new_val));
+29                }
+30            }
+31        }
+32        for(int i=0; i<n; i++){
+33            if(ans[i]>=disappear[i]){
+34                ans[i]=-1;
+35            }
+36        }
+37        return ans;
+38        
+39        
+40
+41    }
+42    class Pair{
+43        int ei;
+44        int val;
+45        public Pair(int ei, int val){
+46            this.ei = ei;
+47            this.val = val;
+48        }
+49    }
+50}
