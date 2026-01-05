83896696ba4c15f80f01fbb95628b0b616e103a5
Srishti Agarwal <srishtiagarwal984@gmail.com>
2026-01-05 14:44:30 +0530

    File updated at 1/5/2026, 2:44:23 PM

--- 2443-SumofNumberandItsReverse/2443-SumofNumberandItsReverse.java
+++ 2443-SumofNumberandItsReverse/2443-SumofNumberandItsReverse.java
@@ -1,24 +1,21 @@
-// Last updated: 1/5/2026, 2:43:20 PM
+// Last updated: 1/5/2026, 2:44:23 PM
 1class Solution {
 2    public boolean sumOfNumberAndReverse(int num) {
-3        if(num==0){
-4            return true;
-5        }
-6        int en =( num+1)/2;
-7        for(int i=1; i<=num; i++){
-8            int r = num-i;
-9            int c=0;
-10            int q = i;
-11            while(q!=0){
-12                c=c*10+q%10;
-13                q=q/10;
-14            }
-15            //System.out.println(i+" "+r);
-16            if(c==r){
-17                return true;
-18            }
-19            
-20        }
-21        return false;
-22    }
-23}
+3        
+4        for(int i=0; i<=num; i++){
+5            int r = num-i;
+6            int c=0;
+7            int q = i;
+8            while(q!=0){
+9                c=c*10+q%10;
+10                q=q/10;
+11            }
+12            //System.out.println(i+" "+r);
+13            if(c==r){
+14                return true;
+15            }
+16            
+17        }
+18        return false;
+19    }
+20}
