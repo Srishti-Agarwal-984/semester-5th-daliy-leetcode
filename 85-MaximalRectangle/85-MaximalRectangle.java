4c5b455142ee2603cb27d22137b135ee2053fa68
Srishti Agarwal <srishtiagarwal984@gmail.com>
2026-01-11 13:59:33 +0530

    File updated at 1/11/2026, 1:59:32 PM

--- 85-MaximalRectangle/85-MaximalRectangle.java
+++ 85-MaximalRectangle/85-MaximalRectangle.java
@@ -1,52 +1,52 @@
-// Last updated: 10/18/2025, 6:05:50 PM
-class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        if(matrix.length == 0) return 0;
-        int n = matrix[0].length;
-        int[] heights = new int[n];
-        int maxArea = 0;
-        
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == '1') heights[j]++;
-                else heights[j] = 0;
-            }
-            maxArea = Math.max(maxArea, Area(heights));
-        }
-        return maxArea;
-    }
-    public int Area(int[] arr){
-        int ans = 0;
-		int c=0;
-		Stack<Integer> st = new Stack<>();
-		for(int i=0; i<arr.length; i++) {
-			while(!st.isEmpty() && arr[i]<arr[st.peek()]) {
-				int h= arr[st.pop()];
-				int r=i;
-				if(st.isEmpty()) {
-					ans=Math.max(h*r, ans);
-				}
-				else {
-					int l= st.peek();
-					int area = h*(r-l-1);
-					ans= Math.max(ans,  area);
-				}
-			}
-			st.push(i);
-		}
-		int r= arr.length;
-		while(!st.isEmpty()) {
-			int h= arr[st.pop()];
-			if(st.isEmpty()) {
-				ans=Math.max(h*r, ans);
-			}
-			else {
-				int l= st.peek();
-				int area = h*(r-l-1);
-				ans= Math.max(ans,  area);
-			}
-			
-		}
-        return ans;
-    }
-}
+// Last updated: 1/11/2026, 1:59:32 PM
+1class Solution {
+2    public int maximalRectangle(char[][] matrix) {
+3        if(matrix.length == 0) return 0;
+4        int n = matrix[0].length;
+5        int[] heights = new int[n];
+6        int maxArea = 0;
+7        
+8        for(int i=0; i<matrix.length; i++){
+9            for(int j=0; j<n; j++){
+10                if(matrix[i][j] == '1') heights[j]++;
+11                else heights[j] = 0;
+12            }
+13            maxArea = Math.max(maxArea, Area(heights));
+14        }
+15        return maxArea;
+16    }
+17    public int Area(int[] arr){
+18        int ans = 0;
+19		int c=0;
+20		Stack<Integer> st = new Stack<>();
+21		for(int i=0; i<arr.length; i++) {
+22			while(!st.isEmpty() && arr[i]<arr[st.peek()]) {
+23				int h= arr[st.pop()];
+24				int r=i;
+25				if(st.isEmpty()) {
+26					ans=Math.max(h*r, ans);
+27				}
+28				else {
+29					int l= st.peek();
+30					int area = h*(r-l-1);
+31					ans= Math.max(ans,  area);
+32				}
+33			}
+34			st.push(i);
+35		}
+36		int r= arr.length;
+37		while(!st.isEmpty()) {
+38			int h= arr[st.pop()];
+39			if(st.isEmpty()) {
+40				ans=Math.max(h*r, ans);
+41			}
+42			else {
+43				int l= st.peek();
+44				int area = h*(r-l-1);
+45				ans= Math.max(ans,  area);
+46			}
+47			
+48		}
+49        return ans;
+50    }
+51}
