ef2830bb3839ca1f48586227fa5d54eb55bb1dcb
Srishti Agarwal <srishtiagarwal984@gmail.com>
2026-01-13 16:55:55 +0530

    File updated at 1/13/2026, 4:55:52 PM

--- 3377-DigitOperationstoMakeTwoIntegersEqual/3377-DigitOperationstoMakeTwoIntegersEqual.java
+++ 3377-DigitOperationstoMakeTwoIntegersEqual/3377-DigitOperationstoMakeTwoIntegersEqual.java
@@ -1,69 +1,47 @@
-// Last updated: 1/13/2026, 4:31:55 PM
-1class Solution {
-2    public int minOperations(int n, int m) {
-3        if(Prime(n) || Prime(m)){
-4            return -1;
-5        }
-6
-7        String start = String.valueOf(n);
-8        String target = String.valueOf(m);
-9
-10        if (start.length() != target.length()) return -1;
-11
-12        PriorityQueue<Pair> pq =
-13            new PriorityQueue<>((a, b) -> a.val - b.val);
-14
-15        pq.add(new Pair(start, n));
-16        HashMap<String, Integer> vi = new HashMap<>();
-17
-18        while (!pq.isEmpty()) {
-19            Pair p = pq.poll();
-20
-21            if (p.s.equals(target)) return p.val;
-22
-23            if (vi.containsKey(p.s) ) continue;
-24            vi.put(p.s, p.val);
-25
-26            for (int i = 0; i < p.s.length(); i++) {
-27                char[] arr = p.s.toCharArray();
-28
-29                // increment
-30                if (arr[i] < '9') {
-31                    arr[i]++;
-32                    String ns = new String(arr);
-33                    if (ns.charAt(0) != '0' && !Prime(Integer.parseInt(ns))) {
-34                        pq.add(new Pair(ns, p.val + Integer.parseInt(ns)));
-35                    }
-36                }
-37
-38                arr = p.s.toCharArray();
-39                // decrement
-40                if (arr[i] > '0') {
-41                    arr[i]--;
-42                    String ns = new String(arr);
-43                    if (ns.charAt(0) != '0' && !Prime(Integer.parseInt(ns))) {
-44                        pq.add(new Pair(ns, p.val +Integer.parseInt(ns) ));
-45                    }
-46                }
-47            }
-48        }
-49        return -1;
-50    }
-51
-52    public boolean Prime(int p) {
-53        if (p <= 1) return false;
-54        for (int i = 2; i * i <= p; i++) {
-55            if (p % i == 0) return false;
-56        }
-57        return true;
-58    }
-59
-60    class Pair {
-61        String s;
-62        int val;
-63        Pair(String s, int val) {
-64            this.s = s;
-65            this.val = val;
-66        }
-67    }
-68}
+// Last updated: 1/13/2026, 4:55:52 PM
+class Solution {
+    private static final int MAX = 10000;
+    private static final boolean[] isPrime = new boolean[MAX + 1];
+    private void init() {
+        if(isPrime[2]) return;
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        int limit = (int)Math.sqrt(MAX);
+        for(int i = 2; i <= limit; i++) {
+            if(isPrime[i]) for(int j = i * i; j <= MAX; j += i) isPrime[j] = false;
+        }
+    }
+    public int minOperations(int n, int m) {
+        init();
+        if(isPrime[n] || isPrime[m]) return -1;
+        int max = 1;
+        while(max <= n) max *= 10;
+        int[] dist = new int[max];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[n] = n;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> dist[a] - dist[b]);
+        pq.offer(n);
+        while(!pq.isEmpty()) {
+            int current = pq.poll();
+            if(current == m) return dist[current];
+            for(int i = 1; i < max; i *= 10) {
+                int digit = current / i % 10;
+                if(digit > 0) {
+                    int next = current - i;
+                    if(next >= i && !isPrime[next] && dist[next] > dist[current] + next) {
+                        dist[next] = dist[current] + next;
+                        pq.offer(next);
+                    }
+                }
+                if(digit < 9) {
+                    int next = current + i;
+                    if(!isPrime[next] && dist[next] > dist[current] + next) {
+                        dist[next] = dist[current] + next;
+                        pq.offer(next);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
