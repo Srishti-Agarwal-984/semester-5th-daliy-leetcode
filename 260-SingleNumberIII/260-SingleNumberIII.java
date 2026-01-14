caeb787a5616d3d67f7bee06ebc5d14658c58dd8
Srishti Agarwal <srishtiagarwal984@gmail.com>
2026-01-14 12:24:32 +0530

    File updated at 1/14/2026, 12:24:31 PM

--- 260-SingleNumberIII/260-SingleNumberIII.java
+++ 260-SingleNumberIII/260-SingleNumberIII.java
@@ -1,20 +1,23 @@
-// Last updated: 1/14/2026, 12:12:16 PM
+// Last updated: 1/14/2026, 12:24:31 PM
 1class Solution {
-2    public int[] singleNumber(int[] nums) {
-3        int[] ans = new int[2];
-4        int j=0, i=0;
-5        Arrays.sort(nums);
-6        while(i<nums.length){
-7            if(i<nums.length-1 &&( nums[i]^nums[i+1])==0){
-8                i=i+2;
-9            }
-10            else{
-11                ans[j]=nums[i];
-12                i++;
-13                j++;
-14            }
-15        }
-16        return ans;
-17        
-18    }
-19}
+2    public int[] singleNumber(int[] arr) {
+3        int c=0;
+4		for(int i: arr) {
+5			c=c^i;
+6		}
+7		int mask = c&(~(c-1));
+8		int a =0, b = 0;
+9		for(int  i: arr) {
+10			if((i&mask)==0) {
+11				a = a^i;
+12			}
+13			else {
+14				b = b^i;
+15			}
+16			
+17		}
+18		int[] ans = {a, b};
+19		return ans;
+20        
+21    }
+22}
