e2df6e395c93dcd835e796005573acc2440dd5cc
Srishti Agarwal <srishtiagarwal984@gmail.com>
2026-01-17 07:47:26 +0530

    File updated at 1/17/2026, 7:47:24 AM

--- 3047-FindtheLargestAreaofSquareInsideTwoRectangles/3047-FindtheLargestAreaofSquareInsideTwoRectangles.java
+++ 3047-FindtheLargestAreaofSquareInsideTwoRectangles/3047-FindtheLargestAreaofSquareInsideTwoRectangles.java
@@ -1,22 +1,24 @@
-// Last updated: 1/17/2026, 7:46:31 AM
-1class Solution {
-2    public long largestSquareArea(int[][] bl, int[][] tr) {
-3        long ans = 0;
-4        for (int i = 0; i < bl.length; i++) {
-5            for (int j = i + 1; j < bl.length; j++) {
-6                long bl1 = Math.max(bl[i][0], bl[j][0]);
-7                long bl2 = Math.max(bl[i][1], bl[j][1]);
-8                long tr1 = Math.min(tr[i][0], tr[j][0]);
-9                long tr2 = Math.min(tr[i][1], tr[j][1]);
-10                long u = Math.min(tr1 - bl1, tr2 - bl2);
-11                if (u < 0) {
-12                    continue;
-13                }
-14                ans = Math.max(ans, u * u);
-15
-16            }
-17        }
-18        return ans;
-19
-20    }
-21}
+// Last updated: 1/17/2026, 7:47:24 AM
+class Solution {
+
+    public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        int n = bottomLeft.length;
+        long maxSide = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int w =
+                    Math.min(topRight[i][0], topRight[j][0]) -
+                    Math.max(bottomLeft[i][0], bottomLeft[j][0]);
+                int h =
+                    Math.min(topRight[i][1], topRight[j][1]) -
+                    Math.max(bottomLeft[i][1], bottomLeft[j][1]);
+                int side = Math.min(w, h);
+
+                maxSide = Math.max(maxSide, side);
+            }
+        }
+
+        return maxSide * maxSide;
+    }
+}
